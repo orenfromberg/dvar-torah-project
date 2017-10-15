ab19f77299ef7340bacf076c430d92c580523a21
Oren Fromberg <oren@adhocteam.us>
2017-10-15 09:02:43 -0400

    Adding express-session.

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,41 @@ let request = require('request-promise');
 
 var router = express.Router();
 
+const checkAuth = (req, res, next) => {
+  if (!req.session.authorization) {
+    res.redirect('/connect');
+  } else {
+    next();
+  }
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'dvar-torah-project' });
+router.get('/', checkAuth, (req, res, next) => {
+  // res.render('index', { title: 'sign up for a dvar.' });
+  // ask reddit for info about the user.
+  request('https://oauth.reddit.com/api/v1/me', {
+    method: 'GET',
+    headers: {
+      'Authorization': `bearer ${req.session.authorization.access_token}`,
+      'User-Agent': 'webapp for the /r/judaism dvar torah project by /r/o_m_f_g'
+    },
+    json: true,
+  })
+  .then((response) => {
+    req.session.me = response;
+    res.render('home', {
+      title: 'register to write a dvar.',
+      name: response.name
+    })
+  })
+  .catch((error) => {
+    console.log(error);
+  })
 });
+
+router.get('/connect', (req, res, next) => {
+  res.render('index', { title: 'connect to reddit.'})
+})
 
 router.get('/authorize', (req, res, next) => {
   const { error, code, state } = req.query;
@@ -28,7 +59,8 @@ router.get('/authorize', (req, res, next) => {
       json: true,
     })
     .then((response) => {
-      res.redirect('/secure');
+      req.session.authorization = response;
+      res.redirect('/');
     })
     .catch((error) => {
       console.log(error);
