dc6c1c30517dadfbaa271ebc077d1b023e5ad89b
Oren Fromberg <oren@adhocteam.us>
2017-10-17 00:14:10 -0400

    Adding hebcal integration.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 var express = require('express');
 let request = require('request-promise');
 
+const { fetchUpcomingParashot } = require('../lib/hebcal');
+
 var router = express.Router();
 
 const checkAuth = (req, res, next) => {
@@ -14,31 +16,41 @@ const checkAuth = (req, res, next) => {
 
 /* GET home page. */
 router.get('/', checkAuth, (req, res, next) => {
-  // res.render('index', { title: 'sign up for a dvar.' });
-  // ask reddit for info about the user.
-  request('https://oauth.reddit.com/api/v1/me', {
+  const options = {
+    uri: 'https://oauth.reddit.com/api/v1/me',
     method: 'GET',
     headers: {
       'Authorization': `bearer ${req.session.authorization.access_token}`,
       'User-Agent': 'webapp for the /r/judaism dvar torah project by /r/o_m_f_g'
     },
     json: true,
-  })
-  .then((response) => {
-    req.session.me = response;
-    res.render('home', {
+  };
+
+  Promise.all([
+    request(options),
+    fetchUpcomingParashot()
+  ])
+  .then(([me, parashot]) => {
+      res.render('home', {
       title: 'register to write a dvar.',
-      name: response.name
+      name: me.name,
+      parashot,
     })
   })
   .catch((error) => {
-    console.log(error);
-  })
+    console.error(error);
+  });
+});
+
+router.post('/submit', (req, res, next) => {
+  const { parashah, name } = req.body;
+
+  // insert into database.
 });
 
 router.get('/connect', (req, res, next) => {
   res.render('index', { title: 'connect to reddit.'})
-})
+});
 
 router.get('/authorize', (req, res, next) => {
   const { error, code, state } = req.query;
